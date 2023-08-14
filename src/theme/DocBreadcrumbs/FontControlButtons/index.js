baa3098b596f8b01d671cfae14dbe8497893e737
mkrastev <matey_krastev2@hotmail.com>
2023-08-14 16:33:38 +0300

    .

--- src/theme/DocBreadcrumbs/FontControlButtons/index.js
+++ src/theme/DocBreadcrumbs/FontControlButtons/index.js
@@ -93,14 +93,14 @@ export function LineHeightButton(){
     )
 }
 
-function LoadFontSettingsFromLocalStorage(){
-    const style = getComputedStyle(document.querySelector(":root"))
-    const storage = window.localStorage
-    style.setProperty("--custom-line-height",storage.getItem("lineHeight"))
-    style.setProperty("--custom-font-weight",storage.getItem("fontWeight"))
-    style.setProperty("--custom-font-factor",storage.getItem("fontSizeFactor"))
-    console.log("styles loaded")
-}
+// function LoadFontSettingsFromLocalStorage(){
+//     const style = getComputedStyle(document.querySelector(":root"))
+//     const storage = window.localStorage
+//     style.setProperty("--custom-line-height",storage.getItem("lineHeight"))
+//     style.setProperty("--custom-font-weight",storage.getItem("fontWeight"))
+//     style.setProperty("--custom-font-factor",storage.getItem("fontSizeFactor"))
+//     console.log("styles loaded")
+// }
 
 export default function FontControlButtons() {
     return (
